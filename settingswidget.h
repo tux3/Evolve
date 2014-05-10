589a80461f40e995d519d43196f87aa3200a27a7
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-05-10 15:57:24 +0200

    Create Settings widget, update GUI

---
+++ settingswidget.h
@@ -0,0 +1,22 @@
+#ifndef SETTINGSWIDGET_H
+#define SETTINGSWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class SettingsWidget;
+}
+
+class SettingsWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsWidget(QWidget *parent = 0);
+    ~SettingsWidget();
+
+private:
+    Ui::SettingsWidget *ui;
+};
+
+#endif // SETTINGSWIDGET_H
